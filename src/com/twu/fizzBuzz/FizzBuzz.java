9af6ccc66418d0d8a62541e7491d1ad0b79938bf
avieira <avieira@thoughtworks.com>
2018-02-07 17:42:57 -0200

    create struture of src and test

---
+++ src/com/twu/fizzBuzz/FizzBuzz.java
@@ -0,0 +1,9 @@
+package com.twu.fizzBuzz;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+
+        System.out.println("test");
+
+    }
+}
---
+++ test/com/twu/fizzBuzz/FizzBuzz.java
@@ -0,0 +1,9 @@
+package com.twu.fizzBuzz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FizzBuzz {
+
+
+}
