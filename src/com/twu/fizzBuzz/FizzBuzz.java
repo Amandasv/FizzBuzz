6b378457183c56ee11278ad123e6c25c1eaa72ce
amandasv <amandasv.pm@gmail.com>
2018-02-09 16:33:09 -0200

    edit main with print class

--- src/com/twu/fizzBuzz/FizzBuzz.java
+++ src/com/twu/fizzBuzz/FizzBuzz.java
@@ -2,15 +2,16 @@ package com.twu.fizzBuzz;
 
 public class FizzBuzz {
     public static void main(String[] args) {
+        Printer printer = new Printer();
 
-        System.out.println("FizzBuzz");
+        printer.print("FizzBuzz");
         countTo(5);
 
     }
 
     public static int countTo(int numberMax) {
-        int counter = 0;
         Printer printer = new Printer();
+        int counter = 0;
 
         for (int position = 1; position <= numberMax ; position++) {
             printer.print(Integer.toString(position));
