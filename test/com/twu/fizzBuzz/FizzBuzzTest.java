ed4395df36f447e15504bded83f3e34cc5a11b3c
amandasv <amandasv.pm@gmail.com>
2018-02-12 14:36:06 -0200

    update tests of methods fizzBuzz

--- test/com/twu/fizzBuzz/FizzBuzzTest.java
+++ test/com/twu/fizzBuzz/FizzBuzzTest.java
@@ -1,24 +1,38 @@
 package com.twu.fizzBuzz;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class FizzBuzzTest {
 
-    private final FizzBuzz fizzBuzz = new FizzBuzz();
+    private final FizzBuzz fizzBuzz = new FizzBuzz(new Printer());
 
     @Test
-    public void countsFromOneToMaxNumber(){
-        int countToHundred = fizzBuzz.countTo(100);
-        Assert.assertEquals(countToHundred, 100);
+    public void returnsFizzIfNumberIsDivisibleByThree() {
+        String message = fizzBuzz.classification(3);
+
+        Assert.assertEquals("Fizz", message);
     }
 
     @Test
-    public void showThisClassification() {
-        String classificationForNumber5 = fizzBuzz.classification(5);
-        Assert.assertEquals(classificationForNumber5, fizzBuzz.classification(5));
+    public void returnsBuzzIfNumberIsDivisibleByFive() {
+        String message = fizzBuzz.classification(5);
 
+        Assert.assertEquals("Buzz", message);
+    }
+
+    @Test
+    public void returnsFizzBuzzIfNumberIsDivisibleByFiveAndThree() {
+        String message = fizzBuzz.classification(15);
+
+        Assert.assertEquals("FizzBuzz", message);
+    }
+
+    @Test
+    public void returnsNumberIfNumberIsNotDivisibleByFiveOrThree() {
+        String message = fizzBuzz.classification(1);
+
+        Assert.assertEquals("1", message);
     }
 
 }
