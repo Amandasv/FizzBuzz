0df7441af4826a47b997458a64abca7535b4dd11
amandasv <amandasv.pm@gmail.com>
2018-02-12 14:33:10 -0200

    edit printer test, verify print of java

--- test/com/twu/fizzBuzz/PrinterTest.java
+++ test/com/twu/fizzBuzz/PrinterTest.java
@@ -1,19 +1,36 @@
 package com.twu.fizzBuzz;
 
-import org.junit.Assert;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class PrinterTest {
 
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(System.out);
+    }
 
     @Test
     public void printsMessagePassed(){
         Printer printer = new Printer();
-        String words = "FizBuzz Test";
+        String words = "FizzBuzz Test";
+
         printer.print(words);
 
-        //need Assert
-
+        assertEquals("FizzBuzz Test", outContent.toString());
     }
 }
